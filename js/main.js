1b95a0e51af05a4da065fb1e90f51ef1b0f5e4d7
RyoYmst <k809231@kansai-u.ac.jp>
2015-02-17 16:27:43 +0900

    first animation
    属性の切り替えができていない

--- js/main.js
+++ js/main.js
@@ -1,51 +1,3 @@
-function LoadJson(){
-  json_data = [];
-  $.ajax({
-    url:"../~artuhr0912/data/data.json",
-    dateType: "json",
-    async:false,
-    data:{"data" : "data"}
-  })
-    .success(function(json){
-      for (var i = 0; i < json.length ; i++){
-        json_data.push(json[i]);
-      }
-    })
-    .error(function(msg){
-      console.log("error(LoadJson)");
-    })
-  return json_data;
-};
-
-
-function ExpectLikeComic(){
-        var hoge = $("textarea[name='titles']").val();
-        var tmp = [];
-        title_list = hoge.split(",");
-        if(title_list.length === 0){
-            $("#test").text("コミックタイトルを入力して下さい");
-        }
-       $.ajax({
-              url:"../~artuhr0912/cgi-bin/jsai_calc.py",
-              type:"POST",
-              dataType:"json",
-              async: false,
-              data: { "query": $("textarea[name='titles']").val() }
-       })
-       .success(function(data){
-                tmp.push(data.title,data.genres);
-                var recommend = $("#recommend");
-                var $node = $("<div>").text(data.title).addClass("center");
-                recommend.append($node);
-
-        })
-       .error(function(msg){
-              console.log(msg);
-              console.log("error");
-        });
-        return tmp;
-}
-
 function CreateNodeGenre(genre){
   var $node = $("<div>").text(genre).addClass("genre");
   return $node;
@@ -66,6 +18,21 @@ function NodeTitle(title){
   return node;
 }
 
+function _NodeTitle(title){
+  var node = $("<div>").text(title).addClass("title");
+  return node;
+}
+
+function NodesTitle(titles){
+  var nodes = []
+  for (var i = 0; i < titles.length; i++){
+      //console.log(titles[i])
+       var node = _NodeTitle(titles[i]);
+      nodes.push(node);
+  }
+  return nodes;
+}
+
 function spread(titles,r){
   for (var i=0;i<titles.length; i++){
     var node = titles[i];
@@ -81,7 +48,7 @@ function spread(titles,r){
 }
 
 
-$(function(){
+$(function(){//main関数
     var DbComic = LoadJson();//収集したコミックのタイトルとジャンル　title: genre:
     $(document).on("click","#button",function(){
         var RecomendComic = ExpectLikeComic();//レコメンドされたコミック
@@ -89,7 +56,6 @@ $(function(){
         var $genre = $("#genres");
         nodes_genre = NodesGenre(recomend_genres)
         for (var i = 0; i< nodes_genre.length; i++){
-          //console.log(nodes_genre[i])
            var each_genre = nodes_genre[i];
            //$genre.append(each_genre);//ジャンル情報をdivに格納
         }
@@ -106,12 +72,10 @@ $(function(){
         }
 
         var $title = $("#titles");
-
         var $expect_like_comic = $(".center");
         var pos = $expect_like_comic.position();
         var centerX = pos.left;
         var centerY = pos.top;
-
         for(var i = 0; i < titles.length; i++){
             var each_title = titles[i];
             $title.append(each_title);
@@ -120,127 +84,60 @@ $(function(){
               top:centerY
             })
          }
-
-    r = 256;
+    r = 128;
     spread(titles,r) 
     $("#like_comic_titles").remove();
   })
 })
 
-
-
 $(document).on("click",".title",function() {
-    //console.log($(this).text();
-    console.log($(this));
+    var DbComic = LoadJson();
+    var this_pos = $(this).position();
+    var thisX = this_pos.left;
+    var thisY = this_pos.top;
+    var center_pos = $(".center").position();
+    var centerX = center_pos.left;
+    var centerY = center_pos.top;
+    var distaceX = centerX - thisX;
+    var distaceY = centerY - thisY;
+
+     $(this).animate({
+       left:"+=" + distaceX,
+       top:"+=" + distaceY },"slow");
+    $("div").removeClass("center");
+    $("#recommend").remove();
+    $(this).removeClass("title");
+    $(this).addClass("center");
+    $(".title").not(this).hide(1000);
+
+    select_comic = []
+    for (var i = 0; i < DbComic.length; i++){
+        if ($(this).text() == DbComic[i].title){
+          select_comic.push(DbComic[i]);
+        }
+    }
+
+    titles = []
+    for (var i=0; i< DbComic.length; i++){
+      for(var j = 0; j < select_comic[0].genres.length; j++){
+        if ($.inArray(select_comic[0].genres[j],DbComic[i].genres) !== -1){
+          titles.push(DbComic[i].title);
+          break
+        }
+      }
+    }
+
+    var node_titles = NodesTitle(titles);  
+    r = 128;
+    spread(node_titles,r)
+
 });
+
 
 $(document).on("click",".genre",function() {
     console.log($(this).text());
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     $(document).on("click",".titles",function(){
-//         $("#expect_like_comic").text($(this).text());
-//         genres = []
-//         for(var i = 0; i < DbComic.length; i++){
-//             if ($(this).text() == DbComic[i].title){
-//           　    $(".genres").remove();
-//                 $(".titles").remove();
-//                 for (var j =0; j < DbComic[i].genres.length ; j++){
-//                     $("<div class = genres>"+DbComic[i].genres[j]+"</div>").appendTo("body");
-//                     genres.push(DbComic[i].genres[j])
-//                 }   
-//             }
-//         }
-
-
-//         for (var i = 0; i< DbComic.length; i++){
-//             for (var j = 0 ; j < genres.length; j ++){
-//                 if ($.inArray(genres[i],DbComic[i].genres) != -1){
-//                     if ($(this).text() != DbComic[i].title){
-//                         $("<div class = titles>"+DbComic[i].title+"</div>").appendTo("body");
-//                         break
-//                     }
-//                 }
-//             }
-//         }
-//     })
-// })
-
-
-//        $.ajax({
-//            url: "data/data.json",
-//            type: "GET",
-//            dataType: "json"
-//        })
-//                   
-//        .success(function(data){
-//            
-//            $.ajax({
-//                url:"../~artuhr0912/cgi-bin/test.py",
-//                type:"POST",
-//                dataType:"text",
-//                data: { 'query': $("textarea[name='titles']").val() }
-//            })
-//             .success(function(data){
-//                      console.log(data);
-//            })
-//             .error(function(msg){
-//                console.log(msg)
-//                console.log('error')
-//            });
-//        })
-//                   
-//        .error(function(msg) {
-//            console.log(msg);
-//        });
-
-
-
-/*
- $(function() {
- $(document).on("click","#button",function(){
- var hoge = $("textarea[name='titles']").val();
- title_list = hoge.split(",");
- 
- $.ajax({
- url: "data/data.json",
- type: "GET",
- dataType: "json"
- })
- .success(function(data){
- var tmp = [];
- for(var i = 0; i < data.length; i++){
- tmp.push(data[i]);
- }
- console.log(title_list);
- for(var i = 0; i< tmp.length; i ++){
- console.log(tmp[i].title)
- }
- })
- .error(function(msg) {
- console.log(msg);
- });
- })
- });
- */
-
-
-
 
 
 
---
+++ js/recommend.js
@@ -0,0 +1,27 @@
+function ExpectLikeComic(){
+        var hoge = $("textarea[name='titles']").val();
+        var tmp = [];
+        title_list = hoge.split(",");
+        if(title_list.length === 0){
+            $("#test").text("コミックタイトルを入力して下さい");
+        }
+       $.ajax({
+              url:"../~artuhr0912/cgi-bin/jsai_calc.py",
+              type:"POST",
+              dataType:"json",
+              async: false,
+              data: { "query": $("textarea[name='titles']").val() }
+       })
+       .success(function(data){
+                tmp.push(data.title,data.genres);
+                var recommend = $("#recommend");
+                var $node = $("<div>").text(data.title).addClass("center");
+                recommend.append($node);
+
+        })
+       .error(function(msg){
+              console.log(msg);
+              console.log("error");
+        });
+        return tmp;
+}

e976de9ad1e6627b233cf6d6b8dbee167ab90267
RyoYmst <k809231@kansai-u.ac.jp>
2015-02-18 11:59:28 +0900

    グラフ遷移

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ function CreateNodeGenre(genre){
   return $node;
 }
 
-
 function NodesGenre(genres){
   var nodes = []
   for (var i = 0; i < genres.length; i++){
@@ -18,20 +17,6 @@ function NodeTitle(title){
   return node;
 }
 
-function _NodeTitle(title){
-  var node = $("<div>").text(title).addClass("title");
-  return node;
-}
-
-function NodesTitle(titles){
-  var nodes = []
-  for (var i = 0; i < titles.length; i++){
-      //console.log(titles[i])
-       var node = _NodeTitle(titles[i]);
-      nodes.push(node);
-  }
-  return nodes;
-}
 
 function spread(titles,r){
   for (var i=0;i<titles.length; i++){
@@ -54,7 +39,7 @@ $(function(){//main関数
         var RecomendComic = ExpectLikeComic();//レコメンドされたコミック
         recomend_genres = RecomendComic[1];
         var $genre = $("#genres");
-        nodes_genre = NodesGenre(recomend_genres)
+        nodes_genre = NodesGenre(recomend_genres);
         for (var i = 0; i< nodes_genre.length; i++){
            var each_genre = nodes_genre[i];
            //$genre.append(each_genre);//ジャンル情報をdivに格納
@@ -90,54 +75,6 @@ $(function(){//main関数
   })
 })
 
-$(document).on("click",".title",function() {
-    var DbComic = LoadJson();
-    var this_pos = $(this).position();
-    var thisX = this_pos.left;
-    var thisY = this_pos.top;
-    var center_pos = $(".center").position();
-    var centerX = center_pos.left;
-    var centerY = center_pos.top;
-    var distaceX = centerX - thisX;
-    var distaceY = centerY - thisY;
-
-     $(this).animate({
-       left:"+=" + distaceX,
-       top:"+=" + distaceY },"slow");
-    $("div").removeClass("center");
-    $("#recommend").remove();
-    $(this).removeClass("title");
-    $(this).addClass("center");
-    $(".title").not(this).hide(1000);
-
-    select_comic = []
-    for (var i = 0; i < DbComic.length; i++){
-        if ($(this).text() == DbComic[i].title){
-          select_comic.push(DbComic[i]);
-        }
-    }
-
-    titles = []
-    for (var i=0; i< DbComic.length; i++){
-      for(var j = 0; j < select_comic[0].genres.length; j++){
-        if ($.inArray(select_comic[0].genres[j],DbComic[i].genres) !== -1){
-          titles.push(DbComic[i].title);
-          break
-        }
-      }
-    }
-
-    var node_titles = NodesTitle(titles);  
-    r = 128;
-    spread(node_titles,r)
-
-});
-
-
-$(document).on("click",".genre",function() {
-    console.log($(this).text());
-
-});
 
 
 
